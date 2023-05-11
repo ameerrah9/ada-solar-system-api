47d7b5bbd9dbc2667323cbe6fcbde828f15d9ab2
Ghameerah (Juh-meer-ruh) McCullers <59162870+ameerrah9@users.noreply.github.com>
2023-05-11 08:41:20 -0400

    Refactor

--- app/__init__.py
+++ app/__init__.py
@@ -3,20 +3,24 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 # import Migrate from flask_migrate
 from flask_migrate import Migrate
+# import libraries
+from dotenv import load_dotenv
+# used to read environment variables
+import os
 
 # give us access to database operations
 # instantiate the db
 db = SQLAlchemy()
 # instantiate the migrate
 migrate = Migrate()
+load_dotenv()
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
     # set up the database
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
-
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
     # connect the db and migrate to our Flask app
     db.init_app(app)
     migrate.init_app(app, db)
